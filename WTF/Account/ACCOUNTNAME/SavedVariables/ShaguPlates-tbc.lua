ddf7e99866a30206a7039c5f92a4604bf14f28d7
Sumsebrum <kyle@xicon.de>
2021-03-05 02:52:03 +0100

    shagu + plates + wtf

---
+++ WTF/Account/ACCOUNTNAME/SavedVariables/ShaguPlates-tbc.lua
@@ -0,0 +1,255 @@
+
+ShaguPlates_profiles = {
+	["ShaguPlates Dark"] = {
+		["appearance"] = {
+			["border"] = {
+				["color"] = "0.0,0.0,0.0,1",
+				["background"] = "0.02,0.02,0.02,0.75",
+			},
+		},
+		["chat"] = {
+			["right"] = {
+				["enable"] = "1",
+			},
+		},
+	},
+	["Adapta"] = {
+		["chat"] = {
+			["right"] = {
+				["enable"] = "1",
+				["height"] = "161",
+				["width"] = "400",
+			},
+			["text"] = {
+				["outline"] = "0",
+			},
+			["global"] = {
+				["custombg"] = "1",
+				["tabmouse"] = "1",
+				["tabdock"] = "0",
+				["border"] = "0,0,0,1",
+				["background"] = "0.15,0.25,0.25,0.3",
+			},
+			["left"] = {
+				["height"] = "161",
+				["width"] = "400",
+			},
+		},
+		["bars"] = {
+			["icon_size"] = "17",
+			["bottomright"] = {
+				["formfactor"] = "4 x 3",
+			},
+			["right"] = {
+				["formfactor"] = "4 x 3",
+			},
+		},
+		["appearance"] = {
+			["infight"] = {
+				["screen"] = "1",
+				["common"] = "0",
+			},
+			["border"] = {
+				["color"] = "0,0,0,1",
+				["background"] = "0.05,0.15,0.15,0.75",
+				["actionbars"] = "4",
+				["unitframes"] = "1",
+			},
+		},
+		["unitframes"] = {
+			["pet"] = {
+				["debuffs"] = "top",
+				["portrait"] = "off",
+				["pheight"] = "6",
+				["pspace"] = "-1",
+				["width"] = "125",
+			},
+			["customcolor"] = "0.1,0.2,0.2,1",
+			["custom"] = "1",
+			["player"] = {
+				["txthpleft"] = "powerdyn",
+				["pheight"] = "6",
+				["portrait"] = "off",
+				["width"] = "225",
+				["height"] = "35",
+				["showPVP"] = "1",
+				["pspace"] = "-1",
+				["panchor"] = "TOPRIGHT",
+			},
+			["custombg"] = "1",
+			["focus"] = {
+				["portrait"] = "off",
+				["pheight"] = "6",
+			},
+			["target"] = {
+				["portrait"] = "off",
+				["pheight"] = "6",
+				["height"] = "35",
+				["width"] = "225",
+				["pspace"] = "-1",
+				["panchor"] = "TOPLEFT",
+			},
+			["ttarget"] = {
+				["portrait"] = "off",
+				["height"] = "16",
+				["pheight"] = "6",
+				["pspace"] = "-1",
+				["width"] = "125",
+			},
+			["tttarget"] = {
+				["portrait"] = "off",
+				["height"] = "16",
+				["pheight"] = "6",
+				["pspace"] = "-1",
+				["width"] = "125",
+			},
+			["ptarget"] = {
+				["portrait"] = "off",
+				["height"] = "8",
+				["pheight"] = "-1",
+				["pspace"] = "-1",
+				["width"] = "125",
+			},
+		},
+	},
+	["ShaguPlates"] = {
+		["chat"] = {
+			["right"] = {
+				["enable"] = "1",
+			},
+		},
+	},
+	["Slim"] = {
+		["panel"] = {
+			["use_unitfonts"] = "1",
+		},
+		["appearance"] = {
+			["infight"] = {
+				["screen"] = "0",
+				["common"] = "1",
+			},
+			["border"] = {
+				["actionbars"] = "1",
+				["chat"] = "2",
+				["default"] = "1",
+				["background"] = "0.1,0.1,0.1,0.8",
+				["color"] = "0,0,0,1",
+				["bags"] = "3",
+				["panels"] = "1",
+				["unitframes"] = "1",
+			},
+		},
+		["tooltip"] = {
+			["position"] = "bottom",
+		},
+		["chat"] = {
+			["right"] = {
+				["enable"] = "1",
+				["height"] = "160",
+				["width"] = "405",
+			},
+			["text"] = {
+				["outline"] = "0",
+			},
+			["global"] = {
+				["custombg"] = "1",
+				["tabmouse"] = "1",
+				["tabdock"] = "0",
+				["border"] = "0,0,0,0",
+				["background"] = "0,0,0,0.3",
+			},
+			["left"] = {
+				["height"] = "160",
+				["width"] = "405",
+			},
+		},
+		["version"] = "3.3.0",
+		["nameplates"] = {
+			["use_unitfonts"] = "1",
+		},
+		["bars"] = {
+			["icon_size"] = "22",
+			["background"] = "0",
+		},
+		["unitframes"] = {
+			["ptarget"] = {
+				["portrait"] = "off",
+				["pheight"] = "-1",
+				["height"] = "8",
+				["pspace"] = "-1",
+				["width"] = "125",
+			},
+			["ttarget"] = {
+				["portrait"] = "off",
+				["pheight"] = "6",
+				["height"] = "16",
+				["pspace"] = "-1",
+				["width"] = "125",
+			},
+			["pet"] = {
+				["pheight"] = "6",
+				["debuffs"] = "top",
+				["pspace"] = "-1",
+				["width"] = "125",
+			},
+			["customcolor"] = "0.15,0.15,0.15,1",
+			["custombgcolor"] = "0.5,0.2,0.2,1",
+			["player"] = {
+				["debuffs"] = "off",
+				["pheight"] = "6",
+				["pspace"] = "-1",
+				["width"] = "225",
+				["txthpleft"] = "powerdyn",
+				["height"] = "35",
+				["buffs"] = "off",
+				["showPVP"] = "1",
+				["panchor"] = "TOPRIGHT",
+			},
+			["custombg"] = "1",
+			["focus"] = {
+				["pheight"] = "6",
+			},
+			["target"] = {
+				["debuffs"] = "off",
+				["width"] = "225",
+				["pspace"] = "-1",
+				["pheight"] = "6",
+				["height"] = "35",
+				["buffsize"] = "14",
+				["panchor"] = "TOPLEFT",
+			},
+			["raid"] = {
+				["height"] = "24",
+				["width"] = "48",
+			},
+			["tttarget"] = {
+				["portrait"] = "off",
+				["pheight"] = "6",
+				["height"] = "16",
+				["pspace"] = "-1",
+				["width"] = "125",
+			},
+			["custom"] = "1",
+		},
+		["buffbar"] = {
+			["pdebuff"] = {
+				["enable"] = "1",
+				["height"] = "14",
+			},
+			["pbuff"] = {
+				["enable"] = "1",
+				["height"] = "14",
+			},
+			["tdebuff"] = {
+				["enable"] = "1",
+				["height"] = "14",
+			},
+		},
+	},
+}
+ShaguPlates_addon_profiles = {
+}
+ShaguPlates_cache = {
+	["libhealth"] = {
+	},
+}
